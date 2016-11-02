c55c20ee3650ab3f435d715b163684c581ba37de
Thorrak <thorrak@excelinspace.com>
2016-11-02 00:07:38 -0400

    Adding v0.6 firmware

--- legacy-platformio/src/EepromManager.cpp
+++ legacy-platformio/src/EepromManager.cpp
@@ -57,9 +57,6 @@ void EepromManager::initializeEeprom()
 	// clear all eeprom
 //	for (uint16_t offset=0; offset<EepromFormat::MAX_EEPROM_SIZE; offset++)
 //		eepromAccess.writeByte(offset, 0);	
-#ifdef ESP8266
-	eepromAccess.set_manual_commit(true);
-#endif
 	zapEeprom();
 
 	deviceManager.setupUnconfiguredDevices();
