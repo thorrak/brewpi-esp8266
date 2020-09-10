f144d19a3ec841ff5b36075d2b0eb8d663f87f16
Scott Peshak <scott@peshak.net>
2020-09-10 13:31:43 -0500

    Add doxygen comments to FilterCascaded and cleanup
    Refactor out NUM_SECTIONS preprocessor macro into a constexpr.  This
    change doesn't effect the compiled code but tightens up the scoping/type
    safety.
    
    Refactored out duplicated code in both the constructor and the
    setCoefficients method.

--- src/FilterCascaded.cpp
+++ src/FilterCascaded.cpp
@@ -25,17 +25,25 @@
 #include "TemperatureFormats.h"
 
 CascadedFilter::CascadedFilter() {
-	for(uint8_t i=0; i<NUM_SECTIONS; i++){
-		sections[i].setCoefficients(2); // default to a b value of 2
-	}	
+  // default to a b value of 2
+  setCoefficients(2);
 }
 
+/**
+ * \brief Set all secition coefficients to a given value
+ *
+ * @param bValue new coefficient value
+ */
 void CascadedFilter::setCoefficients(uint8_t bValue){
-	for(uint8_t i=0; i<NUM_SECTIONS; i++){
+	for(uint8_t i=0; i<numFilterSections; i++){
 		sections[i].setCoefficients(bValue);
 	}
 }
 
+
+/**
+ * \brief Adds a value and returns the most recent filter output
+ */
 temperature CascadedFilter::add(temperature val){
 	temperature_precise valDoublePrecision = tempRegularToPrecise(val);
 	valDoublePrecision = addDoublePrecision(valDoublePrecision);
@@ -43,32 +51,50 @@ temperature CascadedFilter::add(temperature val){
 	return tempPreciseToRegular(valDoublePrecision);
 }
 
+
+/**
+ * \brief Add a precise temperature value to the filter
+ */
 temperature_precise CascadedFilter::addDoublePrecision(temperature_precise val){
 	temperature_precise input = val;
 	// input is input for next section, which is the output of the previous section
-	for(uint8_t i=0; i<NUM_SECTIONS; i++){
+	for(uint8_t i=0; i<numFilterSections; i++){
 		input = sections[i].addDoublePrecision(input);
 	}
 	return input;
 }
 
 
+/**
+ * \brief Get the most recent filter input
+ */
 temperature CascadedFilter::readInput(void){
 	return sections[0].readInput(); // return input of first section
 }
 
+/**
+ * \brief Get the output value
+ */
 temperature_precise CascadedFilter::readOutputDoublePrecision(void){
-	return sections[NUM_SECTIONS-1].readOutputDoublePrecision(); // return output of last section
+	return sections[numFilterSections - 1].readOutputDoublePrecision(); // return output of last section
 }
 
+/**
+ * \brief Get the previous output value
+ */
 temperature_precise CascadedFilter::readPrevOutputDoublePrecision(void){
-	return sections[NUM_SECTIONS-1].readPrevOutputDoublePrecision(); // return previous output of last section
+	return sections[numFilterSections - 1].readPrevOutputDoublePrecision(); // return previous output of last section
 }
 
+
+/**
+ * \brief Initialize filter.
+ *
+ * @param val - Temperature to seed the filter with.
+ */
 void CascadedFilter::init(temperature val){
-	for(uint8_t i=0; i<NUM_SECTIONS; i++){
+	for(uint8_t i=0; i<numFilterSections; i++){
 		sections[i].init(val);
 	}
 }
-
 
